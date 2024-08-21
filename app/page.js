e02b42e64917d93849239730e945c061a9c53025
jellyfish <rvupmo33@gmail.com>
2024-08-21 12:51:52 -0400

    Basic Desktop Styles

--- app/page.js
+++ app/page.js
@@ -1,15 +1,31 @@
+"use client";
+
+import Dashboard from "@/components/Dashboard";
 import Features from "@/components/Features";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 
 export default function Home() {
+  let user = true;
   return (
-    <main className="min-h-screen px-6 py-4">
-      <Header />
-      <Hero />
-      <Features />
-      <Footer />
-    </main>
+    <div
+      className={`${
+        user
+          ? "bg-[#1e1f24] min-h-screen w-full fixed"
+          : "bg-[#09090b] min-h-screen w-full"
+      }`}
+    >
+      {user ? (
+        <Dashboard />
+      ) : (
+        <main className=" min-h-screen w-full max-w-[1200px] m-auto  px-6 py-4">
+          <Header />
+          <Hero />
+          <Features />
+          <Footer />
+        </main>
+      )}
+    </div>
   );
 }
